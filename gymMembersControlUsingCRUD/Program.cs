d962574c6f3a9e451c6e2770be14595eaf1b7e26
Rudy <rudgerylopes@gmail.com>
2025-02-25 20:32:54 +0000

    adding comments

--- gymMembersControlUsingCRUD/Program.cs
+++ gymMembersControlUsingCRUD/Program.cs
@@ -13,7 +13,7 @@ namespace gymMembersControlUsingCRUD
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
 
-            
+            //new comment
         }
     }
 }
